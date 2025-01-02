16b07ce07046b610e6144c60971b4c296c348b4d
Swaroop-siddesh <2311cs010067@mallareddyuniversity.ac.in>
2025-01-02 14:46:59 +0530

    finding 2 greatest values

---
+++ demo2.java
@@ -0,0 +1,13 @@
+public class demo2 {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1= 10;
+        num2= 20;
+        i (num1 > num2){
+            System.out.println("num1 is greater ");
+        }
+        else{
+            System.out.println("num2 is grater ");
+        }
+    }
+}
