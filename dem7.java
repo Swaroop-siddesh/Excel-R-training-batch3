73e73ba3213eb33ac70a00b9a76c2a2e335f1d7f
Swaroop-siddesh <2311cs010067@mallareddyuniversity.ac.in>
2025-01-03 11:11:47 +0530

    demo-7 using scanner for name,age,phone number

---
+++ dem7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class dem7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println(" Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println(" Enter your number: ");
+        String number = scanner.next();
+        scanner.close();
+        }
+    }
