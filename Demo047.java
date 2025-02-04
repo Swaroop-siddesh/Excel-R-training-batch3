8629b57740a13f3a2737c2fd6fe7a1d40a2bf55c
Swaroop-siddesh <2311cs010067@mallareddyuniversity.ac.in>
2025-02-04 12:44:57 +0530

    to count number of digits of a number

---
+++ Demo047.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Demo047{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = sc.nextInt();
+        number = Math.abs(number); 
+        System.out.println("the total number is:");
+        int number = sc.nextInt();
+        
+        Scanner.close();
+    }
+}
