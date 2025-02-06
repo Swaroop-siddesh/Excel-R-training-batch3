5273a6dd7e25720f196a8f2f443217d2fc4ee9b9
Swaroop-siddesh <2311cs010067@mallareddyuniversity.ac.in>
2025-02-06 12:06:46 +0530

    Print 1st, 2nd and last element of an array.

---
+++ Demo063.java
@@ -0,0 +1,15 @@
+package Core_Java;
+
+public class Demo063 {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+        int sum = 0;
+        int n = arr.length;
+        
+        sum += arr[0] + arr[n - 1];
+        System.out.println("Sum of first and last element of array is: "+sum);
+        
+    }
+}
